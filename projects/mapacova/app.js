21902f3c279dc8b7e2b0df8cee199d77356a0310
darzhz <darzhz@protonmail.com>
2024-05-27 17:49:27 +0530

    updated with spacing

--- projects/mapacova/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-	navigator.serviceWorker.register('sw.js')
-		.then((reg) => console.log("serviceWorker registered successfully",reg))
-		.catch((err) => console.log("failed successfully",err));
-}
